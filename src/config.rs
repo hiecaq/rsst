225426e8e67d76ec43b32de7ddc5051245875ed5
quinoa42 <quinoa42@outlook.jp>
2019-03-26 15:51:12 -0700

    format

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,10 @@
-use toml;
 use serde::Deserialize;
 use std::env;
 use std::fs;
 use std::path::PathBuf;
+use toml;
 
-#[derive(Debug,PartialEq)]
+#[derive(Debug, PartialEq)]
 pub enum Error {
     NotSupported,
     NotFound,
@@ -28,7 +28,8 @@ fn to_string(name: &str) -> Result<String, Error> {
         Err(_) => PathBuf::from(match env::var("HOME") {
             Ok(v) => v,
             Err(_) => return Err(Error::NotSupported),
-        }).join(".config"),
+        })
+        .join(".config"),
     };
     match fs::read_to_string(xdg_config_home.join(name).join("config.toml")) {
         Ok(v) => Ok(v),
@@ -52,30 +53,48 @@ mod tests {
 
     #[test]
     fn parse_example() {
-        let config: Config = toml::from_str(r#"
+        let config: Config = toml::from_str(
+            r#"
             [setting]
             output_format = "markdown"
             [source]
             example1 = "https://example.com/rss.xml"
             example2 = "https://example.org/rss.xml"
-        "#).unwrap();
+        "#,
+        )
+        .unwrap();
         assert_eq!(config.setting.output_format, Some(String::from("markdown")));
         assert_eq!(config.source.len(), 2);
-        assert_eq!(config.source.get("example1"), Some(&"https://example.com/rss.xml".to_string()));
-        assert_eq!(config.source.get("example2"), Some(&"https://example.org/rss.xml".to_string()));
+        assert_eq!(
+            config.source.get("example1"),
+            Some(&"https://example.com/rss.xml".to_string())
+        );
+        assert_eq!(
+            config.source.get("example2"),
+            Some(&"https://example.org/rss.xml".to_string())
+        );
     }
 
     #[test]
     fn parse_example_alternative_format() {
-        let config: Config = toml::from_str(r#"
+        let config: Config = toml::from_str(
+            r#"
             setting.output_format = "markdown"
             source.example3 = "https://example.com/rss.xml"
             source.example4 = "https://example.org/rss.xml"
-        "#).unwrap();
+        "#,
+        )
+        .unwrap();
         assert_eq!(config.setting.output_format, Some(String::from("markdown")));
         assert_eq!(config.source.len(), 2);
-        assert_eq!(config.source.get("example3"), Some(&"https://example.com/rss.xml".to_string()));
-        assert_eq!(config.source.get("example4"), Some(&"https://example.org/rss.xml".to_string()));
+        assert_eq!(
+            config.source.get("example3"),
+            Some(&"https://example.com/rss.xml".to_string())
+        );
+        assert_eq!(
+            config.source.get("example4"),
+            Some(&"https://example.org/rss.xml".to_string())
+        );
     }
 
     #[test]
@@ -85,33 +104,47 @@ mod tests {
 
     #[test]
     fn to_string_declared_dir_empty_file() {
-        let fixtures = env::current_dir().expect("failed to get current dir").join("fixtures");
+        let fixtures = env::current_dir()
+            .expect("failed to get current dir")
+            .join("fixtures");
         env::set_var("XDG_CONFIG_HOME", fixtures);
         assert_eq!(to_string("empty"), Ok(String::from("")));
     }
 
     #[test]
     fn to_string_declared_dir_not_exists() {
-        let fixtures = env::current_dir().expect("failed to get current dir").join("fixtures");
+        let fixtures = env::current_dir()
+            .expect("failed to get current dir")
+            .join("fixtures");
         env::set_var("XDG_CONFIG_HOME", fixtures);
         assert_eq!(to_string("NOT_EXISTS"), Err(Error::NotFound));
     }
 
     #[test]
     fn to_string_declared_dir_file_not_exists() {
-        let fixtures = env::current_dir().expect("failed to get current dir").join("fixtures");
+        let fixtures = env::current_dir()
+            .expect("failed to get current dir")
+            .join("fixtures");
         env::set_var("XDG_CONFIG_HOME", fixtures);
         assert_eq!(to_string("nothing"), Err(Error::NotFound));
     }
 
     #[test]
     fn get_simple_example() {
-        let fixtures = env::current_dir().expect("failed to get current dir").join("fixtures");
+        let fixtures = env::current_dir()
+            .expect("failed to get current dir")
+            .join("fixtures");
         env::set_var("XDG_CONFIG_HOME", fixtures);
         let config = get("simple").unwrap();
         assert_eq!(config.setting.output_format, Some(String::from("html")));
         assert_eq!(config.source.len(), 2);
-        assert_eq!(config.source.get("mine"), Some(&"https://quinoa42.github.io/rss.xml".to_string()));
-        assert_eq!(config.source.get("again"), Some(&"quinoa42.github.io/rss.xml".to_string()));
+        assert_eq!(
+            config.source.get("mine"),
+            Some(&"https://quinoa42.github.io/rss.xml".to_string())
+        );
+        assert_eq!(
+            config.source.get("again"),
+            Some(&"quinoa42.github.io/rss.xml".to_string())
+        );
     }
 }
